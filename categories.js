dc9e3fd2f2728d7d8a3a24a467c24e7b0bf4663e
Nikmarsolvad <88890516+Nikmarsolvad@users.noreply.github.com>
2026-02-09 20:26:46 +0100

    Create categories.js

---
+++ categories.js
@@ -0,0 +1,436 @@
+const valisesData = {
+    "INFORMATIQUE": [
+        {
+            "nom": "Ordinateurs portables",
+            "img": "https://m.media-amazon.com/images/I/41M3yHtkRWL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Tablettes tactiles",
+            "img": "https://m.media-amazon.com/images/I/71PbyDuqXeL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Ordinateurs de bureau & Ecrans",
+            "img": "https://m.media-amazon.com/images/I/61vtEiLtYGL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Stockage",
+            "img": "https://m.media-amazon.com/images/I/61B3xFEuqoL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Réseaux",
+            "img": "https://m.media-amazon.com/images/I/71vE8frylpL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Composants pc",
+            "img": "https://m.media-amazon.com/images/I/71myvNbZ7eL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Logiciels",
+            "img": "https://m.media-amazon.com/images/I/617JEA-PKCL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Accessoires Informatique",
+            "img": "https://m.media-amazon.com/images/I/61mb9jQBcuL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Composants et piéces de remplacement",
+            "img": "https://m.media-amazon.com/images/I/81tsKPrKL0L._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Imprimantes et accessoires",
+            "img": "https://m.media-amazon.com/images/I/71lbX76W+PL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Mémoire",
+            "img": "https://m.media-amazon.com/images/G/08/hpp/mands/Samsung_512_Evo_SD._CB799901389_QL95_SY80_.jpg"
+        },
+        {
+            "nom": "Scanners et accessoires",
+            "img": "https://m.media-amazon.com/images/I/61wezHCZynL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Barebones",
+            "img": "https://m.media-amazon.com/images/I/51kfJfW3MIL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Serveurs",
+            "img": "https://m.media-amazon.com/images/I/61AkOrY6UFL._AC_UL320_.jpg"
+        }
+    ],
+    "Livres": [
+        {
+            "nom": "Livres pour enfants",
+            "img": "https://m.media-amazon.com/images/I/71BLCNzqucL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Livres en langues étrangères",
+            "img": "https://m.media-amazon.com/images/I/81CMHBq-IbL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Scolaire et Parascolaire",
+            "img": "https://m.media-amazon.com/images/I/61PK6j5uiTL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Livres de cuisine et vins",
+            "img": "https://m.media-amazon.com/images/I/61eHujiDrZL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Religions et Spiritualités",
+            "img": "https://m.media-amazon.com/images/I/81o5enu4+qL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Sciences humaines",
+            "img": "https://m.media-amazon.com/images/I/91ERQCS64pL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Livres Romans et littérature",
+            "img": "https://m.media-amazon.com/images/I/81jxsUwB4bL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Santé Forme et Diététique",
+            "img": "https://m.media-amazon.com/images/I/71Ywkdoy9GL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Bandes dessinées",
+            "img": "https://m.media-amazon.com/images/I/71s1bCn+cLL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Livres Manga",
+            "img": "https://m.media-amazon.com/images/I/91cTr93aQ6L._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Mystère et suspense",
+            "img": "https://m.media-amazon.com/images/I/81zaTL+zZgL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Sciences Techniques et Médecine",
+            "img": "https://m.media-amazon.com/images/I/71M1Yh4KK6L._AC_UL320_.jpg"
+        }
+    ],
+    "High-Tech": [
+        {
+            "nom": "Téléphones portables et accessoires",
+            "img": "https://m.media-amazon.com/images/I/612NyKjMeLL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Objets connectés",
+            "img": "https://m.media-amazon.com/images/I/61Ey8ZXpW+L._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Audio et vidéo portable",
+            "img": "https://m.media-amazon.com/images/I/718k4WUhCtL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Piles, chargeurs et testeurs",
+            "img": "https://m.media-amazon.com/images/I/71RG3rBV5QL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Tv, vidéo et home cinéma",
+            "img": "https://m.media-amazon.com/images/I/611sJFGh0uL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Photo et caméscopes",
+            "img": "https://m.media-amazon.com/images/I/51wdqmVHlxL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Casques, écouteurs et accessoires",
+            "img": "https://m.media-amazon.com/images/I/61rqHjE9mtL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Audio et Hifi",
+            "img": "https://m.media-amazon.com/images/I/51dxRrtGs1L._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Electronique embarquée",
+            "img": "https://m.media-amazon.com/images/I/71NiKmGh1qL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "GPS et accessoires",
+            "img": "https://m.media-amazon.com/images/I/51UWrvo3QHL._AC_UL320_.jpg"
+        }
+    ],
+    "MAISON & CUISINE": [
+        {
+            "nom": "Aspirateurs, entretien des sols et nettoyeurs de vitres",
+            "img": "https://m.media-amazon.com/images/I/71Veas8E33L._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Patisserie",
+            "img": "https://m.media-amazon.com/images/I/81IC5+bWDgL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Loisirs créatifs",
+            "img": "https://m.media-amazon.com/images/I/91kP3CCPdSL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Ameublement et décoration",
+            "img": "https://m.media-amazon.com/images/I/41KIe43Ex5L._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Tableaux, posters et arts décoratifs",
+            "img": "https://m.media-amazon.com/images/I/71xhEYvc6sL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Couteaux et ustensiles de cuisine",
+            "img": "https://m.media-amazon.com/images/I/813ahm7WWRL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Petit électroménager",
+            "img": "https://m.media-amazon.com/images/I/61CAN0SnJHL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Gros électroménager",
+            "img": "https://m.media-amazon.com/images/I/31l73B30SDL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Café, thé et expresso",
+            "img": "https://m.media-amazon.com/images/I/81pxwp5zaVL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Vaisselle",
+            "img": "https://m.media-amazon.com/images/I/61QUTfInRxL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Chauffage et climatisation",
+            "img": "https://m.media-amazon.com/images/I/71rcaf+x8sL._AC_UL320_.jpg"
+        }
+    ],
+    "Sports & loisirs": [
+        {
+            "nom": "Sacs à dos et sacs de sport",
+            "img": "https://m.media-amazon.com/images/I/61iz5hnE54L._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Football",
+            "img": "https://m.media-amazon.com/images/I/71f6flxYssL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Fitness et musculation",
+            "img": "https://m.media-amazon.com/images/I/717UnbsFCML._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Running",
+            "img": "https://m.media-amazon.com/images/I/715EsSwjo0L._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Cyclisme",
+            "img": "https://m.media-amazon.com/images/I/714PYnTCAIL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Vetements de sport",
+            "img": "https://m.media-amazon.com/images/I/61+f2e95ruL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Electronique",
+            "img": "https://m.media-amazon.com/images/I/51jFJBV92rL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Glisse urbaine",
+            "img": "https://m.media-amazon.com/images/I/713gAvAQPxL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Camping et randonnée",
+            "img": "https://m.media-amazon.com/images/I/51iTeMT0k5L._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Sports nautiques",
+            "img": "https://m.media-amazon.com/images/I/713hBgvL9cL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Chaussures de sport",
+            "img": "https://m.media-amazon.com/images/I/61usAXEYBIL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Sports d'hhiver",
+            "img": "https://m.media-amazon.com/images/I/71bdpjNI8iL._AC_UL320_.jpg"
+        }
+    ],
+    "Musique, DVd et Blu-ray": [
+        {
+            "nom": "CD & Vinyles",
+            "img": "https://m.media-amazon.com/images/I/61oEYdNPlAL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Musique classique",
+            "img": "https://m.media-amazon.com/images/I/71VG8VyCooL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Instruments de musique & Sono",
+            "img": "https://m.media-amazon.com/images/I/71A1BeqjD5L._AC_UL320_.jpg"
+        },
+        {
+            "nom": "DVD & Blu-ray",
+            "img": "https://m.media-amazon.com/images/I/31is8r91r3L._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Séries TV",
+            "img": "https://m.media-amazon.com/images/I/712IoYZLaDL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Prime Video",
+            "img": "https://m.media-amazon.com/images/G/01/signpost/PrimeVideo/Sparkle_80x80_PV_noLocale_1._FMpng_SY80_.png"
+        }
+    ],
+    "Jeux Vidéo, Consoles et Accessoires": [
+        {
+            "nom": "Xbox Digital",
+            "img": "https://m.media-amazon.com/images/I/51OVjV4-GqL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Xbox Series X et S",
+            "img": "https://m.media-amazon.com/images/I/61bs7WiE8YL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Réalité virtuelle",
+            "img": "https://m.media-amazon.com/images/I/61e424BTGoL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Nintendo Switch",
+            "img": "https://m.media-amazon.com/images/G/08/gamingstore25/xsitetraffic/sparkle_gaming_store_._QL95_SY80_.jpg"
+        },
+        {
+            "nom": "PlayStation 5",
+            "img": "https://m.media-amazon.com/images/G/03/Toys/Exclusives/Q4_External_Traffic_Drivers/20231129_HeroImage_NL._FMpng_SY80_.png"
+        },
+        {
+            "nom": "Nintendo Switch 2",
+            "img": "https://m.media-amazon.com/images/I/717JrHodikL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Systèmes de jeux vidéo portables",
+            "img": "https://m.media-amazon.com/images/I/71plwQiRc2L._AC_UL320_.jpg"
+        }
+    ],
+    "Bricolage et jardin": [
+        {
+            "nom": "Quincaillerie",
+            "img": "https://m.media-amazon.com/images/I/61PHdaXcW9L._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Peintures,outils et traitement des murs",
+            "img": "https://m.media-amazon.com/images/I/61Rnjjwy-tL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Electricité",
+            "img": "https://m.media-amazon.com/images/I/51jFJBV92rL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Cuisines et salles de bain",
+            "img": "https://m.media-amazon.com/images/I/81SeksG7Z9L._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Sécurité",
+            "img": "https://m.media-amazon.com/images/G/01/SearchExcellence/QueryTriage/Signpost/amazon-a._CB317591702_FMpng_SY80_.png"
+        },
+        {
+            "nom": "Plomberie",
+            "img": "https://m.media-amazon.com/images/I/61ZLYu7PFqL._AC_UL320_.jpg"
+        }
+    ],
+    "Beauté, santé et bien-etre": [
+        {
+            "nom": "Soin de la Peau",
+            "img": "https://m.media-amazon.com/images/I/71hxzO36LvL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Maquillage",
+            "img": "https://m.media-amazon.com/images/I/51INUScyKML._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Soin des Cheveux",
+            "img": "https://m.media-amazon.com/images/I/81IvoT9ECsL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Soin des Ongles",
+            "img": "https://m.media-amazon.com/images/I/61VwWasfXJL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Parfums",
+            "img": "https://m.media-amazon.com/images/I/61rvUHIaN3L._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Bain, savons et soin du corps",
+            "img": "https://m.media-amazon.com/images/I/71SnlQKBqPL._AC_UL320_.jpg"
+        }
+    ],
+    "Automobile et industrie": [
+        {
+            "nom": "Equipements et fournitures de restauration",
+            "img": "https://m.media-amazon.com/images/I/41wYaSAqlQL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Equipement électrique industriel",
+            "img": "https://m.media-amazon.com/images/I/61F9oqYabwL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Hydraulique, pneumatique et plomberie",
+            "img": "https://m.media-amazon.com/images/I/71xNePSdddL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Outils manuels et électriques",
+            "img": "https://m.media-amazon.com/images/I/81nLar2pyEL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Produits professionnels de sécurité et de santé",
+            "img": "https://m.media-amazon.com/images/I/71gBudpLN-L._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Equipement et fournitures agricoles",
+            "img": "https://m.media-amazon.com/images/I/61qwVEuIq6L._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Equipement de transmission d'énergie",
+            "img": "https://m.media-amazon.com/images/I/51l86tV+GhL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Fournitures de nettoyage et d'entretien",
+            "img": "https://m.media-amazon.com/images/I/61QjreEsd0L._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Energie solaire et éolienne",
+            "img": "https://m.media-amazon.com/images/I/31bvLl0o6dL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Test et mesurage",
+            "img": "https://m.media-amazon.com/images/I/71uQn9fTyKL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Equipement pour magasins et rayonnage",
+            "img": "https://m.media-amazon.com/images/I/612cDOcn72L._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Produits scientifiques et de laboratoire",
+            "img": "https://m.media-amazon.com/images/I/810d1rVbdQL._AC_UL320_.jpg"
+        }
+    ],
+    "Handmade": [
+        {
+            "nom": "Handmade",
+            "img": "https://m.media-amazon.com/images/G/01/SearchExcellence/QueryTriage/Signpost/handmade_2018_signpost._CB499851765_QL95_SY80_.jpg"
+        },
+        {
+            "nom": "Bijoux",
+            "img": "https://m.media-amazon.com/images/I/51iAlN1RyOL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Accessoires, sacs et affaires de voyage",
+            "img": "https://m.media-amazon.com/images/I/91z7Xvg3UyL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Beauty & Grooming",
+            "img": "https://m.media-amazon.com/images/I/71jpfratesL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Fournitures de bureau et artticles de fete",
+            "img": "https://m.media-amazon.com/images/I/81ZVfgoQZQL._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Décoration d'intérieur",
+            "img": "https://m.media-amazon.com/images/I/818V-NK+L1L._AC_UL320_.jpg"
+        },
+        {
+            "nom": "Cuisine et salle a manger",
+            "img": "https://m.media-amazon.com/images/I/610mT2f8KSL._AC_UL320_.jpg"
+        }
+    ]
+};
